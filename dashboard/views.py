a85dceca54e574124758d14cb11a10416dec3056
tatertot365 <tate.gillespie@gmail.com>
2022-12-02 22:09:25 -0700

    timedelta update

--- dashboard/views.py
+++ dashboard/views.py
@@ -128,8 +128,7 @@ def journalPageView(request):
 
 def suggestionsPageView(request):
     currentPerson = Person.objects.get(personID  = request.user.id)
-    today = datetime.now() - timedelta(hours=7)
-    data = JournalEntry.objects.filter(personID = request.user.id, date_recorded = today.date())
+    data = JournalEntry.objects.filter(personID = request.user.id, date_recorded = date.today())
     potassium = 0
     phosphorus = 0
     sodium = 0
@@ -234,8 +233,7 @@ def suggestionsPageView(request):
     return render(request, 'dashboard/suggestions.html', context)
 
 def indexPageView(request):
-    today = datetime.now() - timedelta(hours=7)
-    data = JournalEntry.objects.filter(personID = request.user.id, date_recorded = today.date())
+    data = JournalEntry.objects.filter(personID = request.user.id, date_recorded = date.today())
     potassium = 0
     phosphorus = 0
     sodium = 0
@@ -295,8 +293,8 @@ def indexPageView(request):
         alerts += 'Sugar '
 
     for i in range(1,7) :
-        today = datetime.now() - timedelta(hours=7)
-        new_day = (today.date() - timedelta(days=i))
+        today = date.today()
+        new_day = (today - timedelta(days=i))
         day_data = JournalEntry.objects.filter(personID = request.user.id, date_recorded = new_day)
         day_potassium = 0
         day_phosphorus = 0
